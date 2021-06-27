1d31f9291054bfb779731cd15d360d5e39123498
Todd Resudek <toddresudek@gmail.com>
2021-06-26 20:22:12 -0600

    Use configuration for number of recent links

--- lib/too_short_web/live/link_live.ex
+++ lib/too_short_web/live/link_live.ex
@@ -7,9 +7,17 @@ defmodule TooShortWeb.LinkLive do
 
   alias TooShort.{Link, Links}
 
+  @recent_links 10
+
   @impl true
   def mount(_params, _session, socket) do
-    {:ok, assign(socket, url: "", url_valid?: nil, short_url: "", recent: Links.get_recent())}
+    {:ok,
+     assign(socket,
+       url: "",
+       url_valid?: nil,
+       short_url: "",
+       recent: Links.get_recent(@recent_links)
+     )}
   end
 
   @impl true
@@ -18,7 +26,9 @@ defmodule TooShortWeb.LinkLive do
       {:ok, %Link{short_code: short_code} = new_link} ->
         short_url = Routes.link_url(TooShortWeb.Endpoint, :show, short_code)
         recent = build_recent_list(new_link, socket.assigns.recent)
-        {:noreply, assign(socket, %{short_url: short_url, url: "", url_valid?: nil, recent: recent})}
+
+        {:noreply,
+         assign(socket, %{short_url: short_url, url: "", url_valid?: nil, recent: recent})}
 
       _ ->
         {:noreply, put_flash(socket, :error, "There was an error creating your short link")}
@@ -37,8 +47,9 @@ defmodule TooShortWeb.LinkLive do
   defp build_recent_list(head, list) do
     [head]
     |> Kernel.++(list)
-    |> Enum.take(10)
+    |> Enum.take(@recent_links)
   end
+
   defp validation_class(true), do: "border-green-700"
   defp validation_class(false), do: "border-red-700"
   defp validation_class(_), do: "border-gray-700"
